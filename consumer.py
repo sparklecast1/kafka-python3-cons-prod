4b29dc1e7f2adf36fd9f0f3cbf9474242e7151b8
twy <dmitriy.sonin@nordigy.ru>
2020-01-17 19:25:44 +0300

    first commit

--- consumer.py
+++ consumer.py
@@ -1,5 +1,5 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('rep-topic2', bootstrap_servers='10.62.215.193:9092')
+consumer = KafkaConsumer('rep-topic2', bootstrap_servers='test:9092')
 for msg in consumer:
     print("topic =", msg.topic, "  partition = ", msg.partition, "  value=", msg.value)
     #print(msg)
